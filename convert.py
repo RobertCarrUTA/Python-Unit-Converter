85fe01875d8347d869bb1de9c0214ee136a34da9
Robert Carr <robertcarrcisd@gmail.com>
2022-08-22 21:06:13 -0500

    The window now contains a spinner and closes
    The window now has a spinner and does not close when clicking a button.

--- convert.py
+++ convert.py
@@ -7,10 +7,20 @@ import PySimpleGUI as sg
 # Think of the screen broken up into 3 rows, the first list is the top row (top of the screen).
 # The second list is the middle row of the screen (middle of the screen), and list 3 is the bottom row (bottom of the screen)
 layout = [
-    [sg.Text("Text")],
+    [sg.Text("Text"), sg.Spin(["item 1", "item 2"])],
     [sg.Button("Button")],
     [sg.Input()]
 ]
 
-# Converter is the string for the title parameter for sg.Window
-sg.Window("Converter", layout).read()
+# Converter is the string for the title parameter for sg.Window().
+window = sg.Window("Converter", layout).read()
+
+while True:
+    # .read() listens for any kind of input on the window, so if this is the last
+    # line in the program, once you click a button or something, the program will end
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
